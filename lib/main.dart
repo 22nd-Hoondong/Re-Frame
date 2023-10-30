398d36b2ef8196754e43e668f8054045b1a667d9
falconlee236 <falconlee236@cau.ac.kr>
2023-10-30 18:42:36 +0900

    #1: make default home BottomAppbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:re_frame/Pages/gallery.dart';
+import 'package:re_frame/Widgets/fluid_navbar.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+Color defaultColor = const Color(0xffFFC1B4);
+final PageController pageController = PageController(initialPage: 0);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,57 +17,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: defaultColor),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: PageView(
+        controller: pageController,
+        children: const [
+          Gallery(),
+          Text("world2"),
+          Text("hello3"),
+          Text("world4"),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: (){},
+        child: const Icon(Icons.edit,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: FluidNavBar(
+        pageController: pageController,
+        defaultColor: defaultColor,
       ),
     );
   }
 }
+
