7540332146c1caec5b9008d54066b82becdd4c95
falconlee236 <falconlee236@cau.ac.kr>
2023-11-05 14:06:58 +0900

    #16: calendar page init

---
+++ lib/Pages/calendar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatelessWidget {
+  const Calendar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      locale: "ko_KR",
+      firstDay: DateTime.utc(2021, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: DateTime.now(),
+    );
+  }
+}
