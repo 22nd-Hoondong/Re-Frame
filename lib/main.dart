5422dd8e3d7d984fb7490522ed01a878b091843e
falconlee236 <falconlee236@cau.ac.kr>
2023-11-10 15:46:50 +0900

    #9: add NeverScrollableScrollPhysics in PageView

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class MyHomePage extends StatelessWidget {
       ),
       body: PageView(
         controller: pageController,
+        physics: const NeverScrollableScrollPhysics(), // No sliding
         children: const [
           Gallery(),
           Text("world2"),
