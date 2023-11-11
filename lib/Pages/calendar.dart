b09d7df1cdd11f82a6c690a0d2e401a4872348d9
falconlee236 <falconlee236@cau.ac.kr>
2023-11-11 19:45:35 +0900

    #16: Add Date select Feature in Table_calendar

--- lib/Pages/calendar.dart
+++ lib/Pages/calendar.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime selectedDay = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +22,18 @@ class Calendar extends StatelessWidget {
       locale: "ko_KR",
       firstDay: DateTime.utc(2021, 10, 16),
       lastDay: DateTime.utc(2030, 3, 14),
-      focusedDay: DateTime.now(),
+      focusedDay: focusedDay,
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+        // 선택된 날짜의 상태를 갱신합니다.
+        setState(() {
+          this.selectedDay = selectedDay;
+          this.focusedDay = focusedDay;
+        });
+      },
+      selectedDayPredicate: (DateTime day) {
+        // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
+        return isSameDay(selectedDay, day);
+      },
     );
   }
 }
