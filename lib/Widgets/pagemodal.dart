ebd251b098d8092306574446d255d47c799d2f37
falconlee236 <falconlee236@cau.ac.kr>
2023-11-12 16:34:29 +0900

    #16: Config basic PageModal Layout

--- lib/Widgets/pagemodal.dart
+++ lib/Widgets/pagemodal.dart
@@ -8,28 +8,52 @@ class PostModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: ListView(
+      body: Column(
         children: <Widget>[
-          Text(_post.title),
-          const SizedBox(
-            height: 10,
+          Stack(children: [
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.chevron_left)),
+                  IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.star_border)),
+                ],
+              ),
+            ),
+          ]),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(_post.title),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(_post.content),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text("${_post.date}"),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text("${_post.people}"),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text("${_post.photos}"),
+              ],
+            ),
           ),
-          Text(_post.content),
-          const SizedBox(
-            height: 10,
-          ),
-          Text("${_post.date}"),
-          const SizedBox(
-            height: 10,
-          ),
-          Text("${_post.people}"),
-          const SizedBox(
-            height: 10,
-          ),
-          Text("${_post.photos}"),
         ],
       ),
     );
