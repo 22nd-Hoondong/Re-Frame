30e9d26f88a0227884f8a4aebbe4bcb47c31eab0
hoon <ldh019@naver.com>
2023-11-23 16:26:10 +0900

    #4: rebase onto main

---
+++ lib/Pages/friends.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Friends extends StatefulWidget {
+  const Friends({super.key});
+
+  @override
+  State createState() => _FriendsState();
+}
+
+class _FriendsState extends State<Friends> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
