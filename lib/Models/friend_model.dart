93be5930f84eb1a29f7cd351966c82bea4d136c1
hoon <ldh019@naver.com>
2023-11-27 14:05:00 +0900

    #4: edit friend info model

--- lib/Models/friend_model.dart
+++ lib/Models/friend_model.dart
@@ -1,33 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:re_frame/Models/post_model.dart';
-import 'package:re_frame/Models/photo_model.dart';
 
 class FriendInfo {
-  late List<Photo> home;
   late String name;
+  late String uid;
+  late String email;
 
-  FriendInfo({
-    required this.home,
-    required this.name,
-  });
+  FriendInfo(this.name, this.uid, this.email);
+
+  static FriendInfo fromJson(Map<String, dynamic> e) {
+    return FriendInfo(e['name'], e['id'], e['email']);
+  }
 
   FriendInfo.fromSnapshot(DocumentSnapshot snapshot) {
     Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
-    if (data['home'] != null) {
-      home = List<Photo>.empty(growable: true);
-      data['home'].forEach((photo) {
-        home.add(Photo.fromSnapshot(photo));
-      });
-    }
     name = data['name'];
+    uid = data['uid'];
+    email = data['email'];
   }
 
   Map<String, dynamic> toSnapshot() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    if (home != null) {
-      data['home'] = home.map((photo) => photo.toSnapshot()).toList();
-    }
     data['name'] = name;
+    data['uid'] = uid;
+    data['email'] = email;
     return data;
   }
 }
