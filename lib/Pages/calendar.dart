8259c1723e15db0f6565553646b64ca3fb0b0573
falconlee236 <falconlee236@cau.ac.kr>
2023-12-05 23:14:28 +0900

    #48: check firebaseAuth currentUser when getting Post in calendar

--- lib/Pages/calendar.dart
+++ lib/Pages/calendar.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:re_frame/Widgets/postmodal.dart';
 import 'package:re_frame/calendar_util.dart';
@@ -33,27 +34,31 @@ class _CalendarState extends State<Calendar>
     equals: isSameDay,
     hashCode: getHashCode,
   ); // cascade notation.
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   void getFirebasePosts() async {
     QuerySnapshot<Map<String, dynamic>> querySnapshot =
         await db.collection("posts").get();
     for (var element in querySnapshot.docs) {
       Map<String, dynamic> postData = element.data();
-      Timestamp firebaseDate = postData["date"];
-      DateTime postDate = DateTime.parse(firebaseDate.toDate().toString());
-      Post newPostObj = Post(
-          title: postData["title"],
-          content: postData["content"],
-          date: postDate,
-          people: postData["people"],
-          photos: postData["photos"]);
-      setState(() {
-        if (kEvents.containsKey(postDate)) {
-          kEvents[postDate]!.add(newPostObj);
-        } else {
-          kEvents[postDate] = [newPostObj];
-        }
-      });
+      List<dynamic> peopleList = postData['people'];
+      if (peopleList.contains(_firebaseAuth.currentUser?.uid)) {
+        Timestamp firebaseDate = postData["date"];
+        DateTime postDate = DateTime.parse(firebaseDate.toDate().toString());
+        Post newPostObj = Post(
+            title: postData["title"],
+            content: postData["content"],
+            date: postDate,
+            people: postData["people"],
+            photos: postData["photos"]);
+        setState(() {
+          if (kEvents.containsKey(postDate)) {
+            kEvents[postDate]!.add(newPostObj);
+          } else {
+            kEvents[postDate] = [newPostObj];
+          }
+        });
+      }
     }
   }
 
