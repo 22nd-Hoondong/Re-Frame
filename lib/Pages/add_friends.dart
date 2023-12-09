b5d8d2a784f494be9835de43586bd074f2f1c243
hoon <ldh019@naver.com>
2023-12-09 17:22:30 +0900

    hotfix: Appbar background error

--- lib/Pages/add_friends.dart
+++ lib/Pages/add_friends.dart
@@ -21,73 +21,84 @@ class _AddFriendsState extends State<AddFriends> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: pointColor,
-        title: const Text('친구 추가'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        )
-      ),
-        body: Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SearchAnchor(
-              isFullScreen: false,
-              builder: (BuildContext context, SearchController controller) {
-                return SearchBar(
-                  controller: controller,
-                  padding: const MaterialStatePropertyAll<EdgeInsets>(
-                      EdgeInsets.symmetric(horizontal: 16.0)),
-                  onChanged: (_) {
-                    setState(() {
-                      _searchText = controller.text;
-                      AddFriends.bloc.searchFriends(_searchText);
-                    });
-                  },
-                  leading: const Icon(Icons.search),
-                );
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    pointColor,
+                    backgroundColor,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+            title: const Text('친구 추가'),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
               },
-              suggestionsBuilder:
-                  (BuildContext context, SearchController controller) {
-                return List.empty();
-              }),
-        ),
-        Expanded(
-            child: StreamBuilder(
-          stream: AddFriends.bloc.stream,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (_searchText.isEmpty) {
-              return const Center(child: Text('검색어를 입력해주세요'));
-            } else if (snapshot.hasData) {
-              var result = snapshot.data as List<FriendInfo>;
-              return ListView.builder(
-                  itemCount: result.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      title: Text(result[index].name),
-                      subtitle: Text(result[index].email),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () {
-                          AddFriends.bloc.addFriend(result[index]);
+            )),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SearchAnchor(
+                  isFullScreen: false,
+                  builder: (BuildContext context, SearchController controller) {
+                    return SearchBar(
+                      controller: controller,
+                      padding: const MaterialStatePropertyAll<EdgeInsets>(
+                          EdgeInsets.symmetric(horizontal: 16.0)),
+                      onChanged: (_) {
+                        setState(() {
+                          _searchText = controller.text;
                           AddFriends.bloc.searchFriends(_searchText);
-                          Friends.bloc.getFriends();
-                        },
-                      ),
+                        });
+                      },
+                      leading: const Icon(Icons.search),
                     );
-                  });
-            } else if (snapshot.hasError) {
-              return const Center(child: Text('Error'));
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        )),
-      ],
-    ));
+                  },
+                  suggestionsBuilder:
+                      (BuildContext context, SearchController controller) {
+                    return List.empty();
+                  }),
+            ),
+            Expanded(
+                child: StreamBuilder(
+              stream: AddFriends.bloc.stream,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (_searchText.isEmpty) {
+                  return const Center(child: Text('검색어를 입력해주세요'));
+                } else if (snapshot.hasData) {
+                  var result = snapshot.data as List<FriendInfo>;
+                  return ListView.builder(
+                      itemCount: result.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(result[index].name),
+                          subtitle: Text(result[index].email),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.add),
+                            onPressed: () {
+                              AddFriends.bloc.addFriend(result[index]);
+                              AddFriends.bloc.searchFriends(_searchText);
+                              Friends.bloc.getFriends();
+                            },
+                          ),
+                        );
+                      });
+                } else if (snapshot.hasError) {
+                  return const Center(child: Text('Error'));
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            )),
+          ],
+        ));
   }
 }
