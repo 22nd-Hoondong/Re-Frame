270c108cf05b66e88368acb5e0df708217e97dee
falconlee236 <falconlee236@cau.ac.kr>
2023-12-11 11:15:45 +0900

    #60: remove unnecessary import in setting.dart

--- lib/Pages/setting.dart
+++ lib/Pages/setting.dart
@@ -1,9 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:re_frame/main.dart';
-import 'package:re_frame/Pages/login.dart';
 
 class Setting extends StatefulWidget {
   const Setting({super.key});
@@ -13,7 +12,6 @@ class Setting extends StatefulWidget {
 }
 
 class _SettingState extends State<Setting> with MyHomePageStateMixin {
-
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   User? loggedUser;
 
@@ -23,13 +21,13 @@ class _SettingState extends State<Setting> with MyHomePageStateMixin {
     getCurrentUser();
   }
 
-  void getCurrentUser(){
-    try{
+  void getCurrentUser() {
+    try {
       final user = _firebaseAuth.currentUser;
-      if(user != null){
+      if (user != null) {
         loggedUser = user;
       }
-    } catch(e){
+    } catch (e) {
       print(e);
     }
   }
@@ -42,34 +40,50 @@ class _SettingState extends State<Setting> with MyHomePageStateMixin {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-              child: Text('${loggedUser!.displayName}', style: TextStyle(fontSize: 30, color: Colors.black,))
+              child: Text('${loggedUser!.displayName}',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                  ))),
+          SizedBox(
+            height: 10,
           ),
-          SizedBox(height: 10,),
           Container(
-              child: Text('${loggedUser!.email}', style: TextStyle(fontSize: 15, color: Colors.black,))
+              child: Text('${loggedUser!.email}',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                  ))),
+          SizedBox(
+            height: 20,
           ),
-          SizedBox(height: 20,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              OutlinedButton(onPressed: (){
-                signOutGoogle();
-              }, child: const Text('로그아웃'),
-              style: OutlinedButton.styleFrom(
-                primary: Colors.black,
-                backgroundColor: Colors.white,
-                shadowColor: Colors.grey,
-                elevation: 3,
-                side: BorderSide(color: Colors.transparent),
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                ),
-              )),
-              SizedBox(width: 30,),
-              OutlinedButton(onPressed: (){
-                deleteAccount();
-              }, child: const Text('회원 탈퇴'),
+              OutlinedButton(
+                  onPressed: () {
+                    signOutGoogle();
+                  },
+                  child: const Text('로그아웃'),
+                  style: OutlinedButton.styleFrom(
+                    primary: Colors.black,
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.grey,
+                    elevation: 3,
+                    side: BorderSide(color: Colors.transparent),
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  )),
+              SizedBox(
+                width: 30,
+              ),
+              OutlinedButton(
+                  onPressed: () {
+                    deleteAccount();
+                  },
+                  child: const Text('회원 탈퇴'),
                   style: OutlinedButton.styleFrom(
                     primary: Colors.black,
                     backgroundColor: Colors.white,
@@ -83,8 +97,13 @@ class _SettingState extends State<Setting> with MyHomePageStateMixin {
                   )),
             ],
           ),
-          SizedBox(height: 20,),
-          Text('ⓒ 2023. RE:Frame All Rights Reserved.', style: TextStyle(color: Colors.grey,)),
+          SizedBox(
+            height: 20,
+          ),
+          Text('ⓒ 2023. RE:Frame All Rights Reserved.',
+              style: TextStyle(
+                color: Colors.grey,
+              )),
         ],
       ),
     );
